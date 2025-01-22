3bde6845519503b8a728e00c0ba59738050c90d0
FatimaFajardo <fatimafajardotrabajo@gmail.com>
2025-01-22 19:08:51 +0100

    Primer ejercicio de metaetiquetas
    Creamos la página unica de php

---
+++ php/unica.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset ="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+        <title>Esto es un title</title>
+        <meta name="description" content="Esto es una descripción">
+    </head>
+    <body>
+        <header>
+
+        </header>
+        <article>
+            <h1>Unica</h1>
+            <p>Esta es una página nueva </p>
+        </article>
+        <footer>
+        
+        </footer>
+    </body>
+
+    
