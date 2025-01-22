2d131bf1999c2e9bde774f4fb3c1bd9cbdedcecd
FatimaFajardo <fatimafajardotrabajo@gmail.com>
2025-01-22 20:54:03 +0100

    Ejercicio metarefresh

--- php/unica.php
+++ php/unica.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="es">
     <head>
+        <meta http-equiv="refresh" content="30; url=https://www.google.com">
         <meta charset ="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="utf-8">
         <title>Esto es un title</title>
         <meta name="description" content="Esto es una descripción">
         <meta name="googlebot" content="index, follow, max-image-preview: none, max-snipet: 25, unavailable_after: 2033-09-27" >
-
+        <meta name="rating" content="adult">
     </head>
     <body>
         <header>
